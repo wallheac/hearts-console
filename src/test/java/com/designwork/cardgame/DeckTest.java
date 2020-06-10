514730ee1fea0a3dd18839674f6a7a09899610f0
wallheac <wallheac@gmail.com>
2020-06-10 17:53:13 -0500

    Test dealing of 3 and 5 player games

--- src/test/java/com/designwork/cardgame/DeckTest.java
+++ src/test/java/com/designwork/cardgame/DeckTest.java
@@ -1,12 +1,29 @@
 package com.designwork.cardgame;
 
+import com.designwork.cardgame.player.PlayerModel;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
 public class DeckTest {
+    private PlayerModel playerModel;
+
+    @Before
+    public void setup() {
+        playerModel = new PlayerModel("Me");
+    }
+
+    @After
+    public void teardown() {
+        Deck.Deck().resetDeck();
+    }
 
     @Test
     public void shuffleCardsShufflesCards() {
@@ -25,6 +42,24 @@ public class DeckTest {
         String reset = Deck.Deck().toString();
 
         assertThat(reset.split(",").length, is(52));
+    }
+
+    @Test
+    public void dealGivesPlayers10CardsWhen5Players() {
+        List<PlayerModel> playerModels = Arrays.asList(playerModel, playerModel, playerModel, playerModel, playerModel);
+
+        Deck.Deck().deal(playerModels);
+
+        assertThat(playerModel.getHand().size(), is(10));
+    }
+
+    @Test
+    public void dealGivesPlayers17CardsWhen3Players() {
+        List<PlayerModel> playerModels = Arrays.asList(playerModel, playerModel, playerModel);
+
+        Deck.Deck().deal(playerModels);
+
+        assertThat(playerModel.getHand().size(), is(17));
     }
 
 }
